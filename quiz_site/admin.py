e81e1d68f820b91e06fcdab848e5c9bbb897ed63
DavidHearl <david.hearl@outlook.com>
2024-05-26 20:51:21 +0100

    added the basic strucutre

--- quiz_site/admin.py
+++ quiz_site/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from .models import User, Quiz, QuestionCategory, PictureQuestion, MultipleChoice, TrueFalseQuestion
+
+class UserAdmin(admin.ModelAdmin):
+    list_display = (
+        'player_name',
+        'player_score'
+    )
 
 # Register your models here.
+admin.site.register(User, UserAdmin)
+admin.site.register(Quiz)
+admin.site.register(QuestionCategory)
+admin.site.register(PictureQuestion)
+admin.site.register(MultipleChoice)
+admin.site.register(TrueFalseQuestion)
--- quiz_site/models.py
+++ quiz_site/models.py
@@ -1,48 +1,73 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 
 # Create your models here.
 class User(models.Model):
     player_name = models.CharField(max_length=100)
-    player_score = models.IntegerField()
+    player_score = models.IntegerField(null=True, blank=True)
+
+    def __str__(self):
+        return self.player_name
 
 
 class Quiz(models.Model):
     players = models.ManyToManyField(User)
 
+    def __str__(self):
+        return self.players
+
 
 class QuestionCategory(models.Model):
     category = models.CharField(max_length=100)
 
+    def __str__(self):
+        return self.category
+
 
 # Questions
 class PictureQuestion(models.Model):
-    question = models.CharField()
-    answer = models.CharField()
+    category = models.ForeignKey(QuestionCategory, on_delete=models.CASCADE,null=True,  blank=True)
+    question = models.CharField(max_length=100)
+    answer = models.CharField(max_length=100)
     difficulty = models.FloatField()
     image = models.ImageField()
-    created_by = model.ForeignKey(User)
+    created_by = models.ForeignKey(User, on_delete=models.CASCADE)
 
     choice_1 = models.CharField(max_length=100)
     choice_2 = models.CharField(max_length=100)
     choice_3 = models.CharField(max_length=100)
     choice_4 = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.question
 
 
 class MultipleChoice(models.Model):
-    question = models.CharField()
-    answer = models.CharField()
+    category = models.ForeignKey(QuestionCategory, on_delete=models.CASCADE,null=True,  blank=True)
+    question = models.CharField(max_length=100)
+    answer = models.CharField(max_length=100)
     difficulty = models.FloatField()
-    created_by = model.ForeignKey(User)
+    created_by = models.ForeignKey(User, on_delete=models.CASCADE)
 
     choice_1 = models.CharField(max_length=100)
     choice_2 = models.CharField(max_length=100)
     choice_3 = models.CharField(max_length=100)
     choice_4 = models.CharField(max_length=100)
 
+    def __str__(self):
+        return self.question
+
 
 class TrueFalseQuestion(models.Model):
+    category = models.ForeignKey(QuestionCategory, on_delete=models.CASCADE,null=True,  blank=True)
     question = models.CharField(max_length=100)
+    answer = models.CharField(max_length=100)
+    difficulty = models.FloatField()
+    created_by = models.ForeignKey(User, on_delete=models.CASCADE)
+
     true_statement = models.CharField(max_length=100)
     false_statement = models.CharField(max_length=100)
-    answer = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.question
+
