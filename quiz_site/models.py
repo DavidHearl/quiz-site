a1dd4dba91a89e0984517abcd892199094b2c6c8
DavidHearl <david.hearl@outlook.com>
2024-05-27 17:14:26 +0100

    fixed error with the string return

--- quiz_site/models.py
+++ quiz_site/models.py
@@ -40,7 +40,7 @@ class PictureQuestion(models.Model):
     choice_4 = models.CharField(max_length=100, null=True,  blank=True)
 
     def __str__(self):
-        return self.category
+        return f"{self.category.category} - {self.answer}"
 
 
 class MultipleChoice(models.Model):
@@ -55,7 +55,7 @@ class MultipleChoice(models.Model):
     choice_4 = models.CharField(max_length=100, null=True,  blank=True)
 
     def __str__(self):
-        return self.category
+        return f"{self.category.category} - {self.answer}"
 
 
 # True or False Questions
