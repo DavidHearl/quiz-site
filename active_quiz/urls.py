85258c1aeb64bf633f94fff10395f2ed1bab5b82
DavidHearl <david.hearl@outlook.com>
2024-11-22 12:54:35 +0000

    fixed issue with the general knowledge quiz not behaving correctly

--- active_quiz/urls.py
+++ active_quiz/urls.py
@@ -6,8 +6,7 @@ app_name = 'active_quiz'
 
 urlpatterns = [
     path('', views.active_quiz, name='active_quiz'),
-    path('next_flag/', views.next_flag, name='next_flag'),
-    path('previous_flag/', views.previous_flag, name='previous_flag'),
     path('check_update/', views.check_update, name='check_update'),
+    path('next_flag/', views.next_flag, name='next_flag'),
     path('next_general_knowledge/', views.next_general_knowledge, name='next_general_knowledge'),
 ]
--- active_quiz/views.py
+++ active_quiz/views.py
@@ -8,19 +8,21 @@ import random
 
 def active_quiz(request):
     quiz = Quiz.objects.latest('date_created')
-
     # Get the total number of questions in each round
     total_flags = len(quiz.random_numbers.get("Flags", []))
     total_gk = len(quiz.random_numbers.get("General Knowledge", []))
-
     # Determine the current round based on the question counter
     if quiz.question_counter < total_flags:
         current_round = "Flags"
     elif quiz.question_counter < total_flags + total_gk:
         current_round = "General Knowledge"
+        # Reset gk_answered when transitioning to General Knowledge round
+        if request.session.get('last_round') != "General Knowledge":
+            request.session['gk_answered'] = False
     else:
         current_round = None
-
+    # Store the current round in the session
+    request.session['last_round'] = current_round
     # Get the current question index within the current round
     if current_round == "Flags":
         current_index = quiz.question_counter % total_flags
@@ -45,7 +47,6 @@ def active_quiz(request):
         choices = []
         current_question = None
         gk_choices = []
-
     context = {
         'quiz': quiz,
         'current_flag': current_flag,
@@ -110,27 +111,14 @@ def next_general_knowledge(request):
             player.incorrect_answers = (player.incorrect_answers or 0) + 1
             if request.user.username != 'david':
                 messages.error(request, 'Incorrect answer.')
+
         player.save()
+
         if request.user.username == 'david':
             quiz = Quiz.objects.latest('date_created')
             quiz.question_counter += 1
             quiz.save()
             request.session['last_question_counter'] = quiz.question_counter
-    return redirect('active_quiz:active_quiz')
-
-
-@login_required
-def previous_flag(request):
-    current_flag_index = request.session.get('current_flag_index', 0)
-    current_flag_index = max(0, current_flag_index - 1)
-    request.session['current_flag_index'] = current_flag_index
-
-    if request.user.username == 'david':
-        quiz = Quiz.objects.latest('date_created')
-        quiz.question_counter = max(0, quiz.question_counter - 1)
-        quiz.save()
-        request.session['last_question_counter'] = quiz.question_counter
 
     return redirect('active_quiz:active_quiz')
-
 
